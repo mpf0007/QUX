a596f3d380a84dd96f5812142929353cf4e3235e
pouya.farahany <m.p.farahany@gmail.com>
2023-08-31 19:46:50 +0330

    add example

--- README.md
+++ README.md
@@ -3,3 +3,24 @@
 </p>
 
 <p align=center  style="background-color: #1a1b1c; color: #c3a268; " > Browser Engine </p>
+
+Instructions
+------------
+
+1. [Install Rust](http://www.rust-lang.org/install.html)
+
+2. Clone the QUX source code from https://github.com/mpf0007/QUX
+
+3. Run `cargo build` to build QUX, and `cargo run` to run it.
+
+To build and run with optimizations enabled, use `cargo build --release` and
+`cargo run --release`.
+
+By default, QUX will load test.html and test.css from the `examples`
+directory.  You can use the `--html` and `--css` arguments to the QUX
+executable to change the input files:
+
+    ./target/debug/QUX --html examples/test.html --css examples/test.css
+
+The rendered page will be saved to a file named `output.pdf`.  To change the output filename, use the `-o` option.  To switch to PNG output, use add
+`--format png`.
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,72 @@
 #[warn(non_snake_case)]
+extern crate getopts;
+
+use std::default::Default;
+use std::fs::File;
+use std::io::{BufWriter, Read};
+
 pub mod css;
 pub mod dom;
 pub mod html;
 pub mod layout;
 pub mod painting;
+pub mod pdf;
 pub mod style;
 
 fn main() {
-    println!("Hello, world!");
+    // Parse command-line options:
+    let mut opts = getopts::Options::new();
+    opts.optopt("h", "html", "HTML document", "FILENAME");
+    opts.optopt("c", "css", "CSS stylesheet", "FILENAME");
+    opts.optopt("o", "output", "Output file", "FILENAME");
+    opts.optopt("f", "format", "Output file format", "png | pdf");
+
+    let matches = opts.parse(std::env::args().skip(1)).unwrap();
+    let str_arg = |flag: &str, default: &str| -> String {
+        matches.opt_str(flag).unwrap_or(default.to_string())
+    };
+
+    // Choose a format:
+    let pdf = match &str_arg("f", "pdf")[..] {
+        "pdf" => true,
+        "png" => false,
+        x => panic!("Unknown output format: {}", x),
+    };
+
+    // Read input files:
+    let html = read_source(str_arg("h", "examples/test.html"));
+    let css = read_source(str_arg("c", "examples/test.css"));
+
+    // Since we don't have an actual window, hard-code the "viewport" size.
+    let mut viewport: layout::Dimensions = Default::default();
+    viewport.content.width = 800.0;
+    viewport.content.height = 600.0;
+
+    // Parsing and rendering:
+    let root_node = html::parse(html);
+    let stylesheet = css::parse(css);
+    let style_root = style::style_tree(&root_node, &stylesheet);
+    let layout_root = layout::layout_tree(&style_root, viewport);
+
+    // Create the output file:
+    let filename = str_arg("o", if pdf { "output.pdf" } else { "output.png" });
+    let mut file = BufWriter::new(File::create(&filename).unwrap());
+
+    // Write to the file:
+    let ok = pdf::render(&layout_root, viewport.content, &mut file).is_ok();
+
+    if ok {
+        println!("Saved output as {}", filename)
+    } else {
+        println!("Error saving output as {}", filename)
+    }
+}
+
+fn read_source(filename: String) -> String {
+    let mut str = String::new();
+    File::open(filename)
+        .unwrap()
+        .read_to_string(&mut str)
+        .unwrap();
+    str
 }
